f20b4edc15c1fbfdb16feb07b6eae2b71a5af31f
Vít Ondruch <v.ondruch@tiscali.cz>
2013-03-11 17:33:19 +0100

    Prevent failure when "prefix" is empty string.
    In case that "prefix" is empty string, the config_string returns false. Default to empty string for that case.

--- ext/RMagick/extconf.rb
+++ ext/RMagick/extconf.rb
@@ -78,7 +78,7 @@ end
 # binaries). This causes problems when /usr/lib is in the path (e.g., using
 # the default Ruby installation).
 def check_partial_imagemagick_versions()
-   prefix = config_string("prefix")
+   prefix = config_string("prefix") || ""
    matches = [
      prefix+"/lib/lib?agick*",
      prefix+"/include/ImageMagick",
